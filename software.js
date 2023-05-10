24f1d0573a781538c0abaa29790a2da55f3c815f
Addukhankhan <ka862657@gmail.com>
2023-05-10 19:59:39 +0100

    add

--- software.js
+++ software.js
@@ -4,7 +4,5 @@ console.log("hello world");
 console.log("This is Aavez");
 console.log("hello im adnan");
 console.log("hello i m zeeshanali");
-<<<<<<< HEAD
 console.log("im adnan");
-=======
->>>>>>> f48e3b05e4c9e03569e8eaba5d693df4dec55a8b
+console.log ("okk");
